df98e0a64b8a774e42e58f15935ce1c323d90731
alvaroquintero28 <162182742+alvaroquintero28@users.noreply.github.com>
2024-04-15 20:56:57 -0500

    Update app.py

--- penguins/app.py
+++ penguins/app.py
@@ -1,170 +1,130 @@
-# ALWAYS familiarize yourself with the dataset you are working with first.
+# ALWAYS familiarize yourself with the dataset you are working with first
 
-import faicons as fa  # For using font awesome in cards
-import palmerpenguins  # This package provides the Palmer Penguins dataset
-import pandas as pd  # Pandas for data manipulation, required by plotly.express
-import plotly.express as px  # Plotly Express for making Plotly plots
-import seaborn as sns  # Seaborn for making Seaborn plots
-from shinywidgets import render_plotly  # For rendering Plotly plots
-from shiny import reactive, render, req  # To define reactive calculations
-from shiny.express import input, ui  # To define the user interface
+import seaborn as sns
+from faicons import icon_svg
 
+from shiny import reactive
+from shiny.express import input, render, ui
+import palmerpenguins
+import importlib
+import plotly.express as px
+from shinywidgets import render_plotly
 
-
-penguins_df = palmerpenguins.load_penguins()
+df = palmerpenguins.load_penguins()
 
 # This is where it can get tricky
-ui.page_opts(
-    title="PyShiny Express: Palmer Penguins Example", 
-    fillable=True
-    )
+
+ui.page_opts(title="Alvaro King Penguins", fillable=True)
 
 #make sure punctuation marks and indentions are correct
 
-with ui.sidebar(open="open"):
-
-    ui.h2("Sidebar")
-
-    ui.input_selectize(
-        "selected_attribute",
-        "Select Plotly Attribute",
-        ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g", "year"],
-    )
-
-    ui.input_numeric("plotly_bin_count", "Number of Plotly bins", 30)
-
-    ui.input_slider("seaborn_bin_count", "Number of Seaborn bins", 1, 100, 20)
-
-    ui.hr()
-
+with ui.sidebar(title="Penguin Dashboard Filters"):
+    ui.input_slider("mass", "Mass", 2000, 6000, 6000)
     ui.input_checkbox_group(
-        "selected_species",
-        "Species in Scatterplot",
+        "species",
+        "Species",
         ["Adelie", "Gentoo", "Chinstrap"],
         selected=["Adelie", "Gentoo", "Chinstrap"],
-        inline=True,
     )
-
     ui.hr()
-    ui.h6("Links:")
+    ui.h6("Links")
     ui.a(
         "GitHub Source",
-        href="https://github.com/denisecase/pyshiny-penguins-dashboard-express",
+        href="https://alvaroquintero28.github.io/cintel-07/",
         target="_blank",
     )
     ui.a(
         "GitHub App",
-        href="https://denisecase.github.io/pyshiny-penguins-dashboard-express/",
+        href="https://alvaroquintero28.github.io/cintel-07/",
+        target="_blank",
+    )
+    ui.a(
+        "GitHub Issues",
+        href="https://github.com/alvaroquintero28/cintel-07/issues/2",
         target="_blank",
     )
     ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
     ui.a(
-        "PyShiny Express",
-        href="hhttps://shiny.posit.co/blog/posts/shiny-express/",
+        "Template: Basic Dashboard",
+        href="https://shiny.posit.co/py/templates/dashboard/",
         target="_blank",
     )
+    ui.input_radio_buttons("dark_mode", "Dark Mode:", ["Yes", "No"], selected="No")
+    
     ui.a(
-        "See the Code",
-        href="https://shiny.posit.co/py/docs/user-interfaces.html#basic-dashboard",
+        "See also",
+        href="https://github.com/denisecase/pyshiny-penguins-dashboard-express",
         target="_blank",
     )
-    ui.a(
-        "Output: DataGrid",
-        href="https://shiny.posit.co/py/components/outputs/datatable/",
-        target="_blank",
-    )
-    ui.a(
-        "Output: DataTable",
-        href="https://shiny.posit.co/py/components/outputs/datatable/",
-        target="_blank",
-    )
-    ui.a(
-        "Output: Plotly Scatterplot",
-        href="https://shiny.posit.co/py/components/outputs/plot-plotly/",
-        target="_blank",
-    )
-    ui.a(
-        "Output: Seaborn Histogram",
-        href="https://shiny.posit.co/py/components/outputs/plot-seaborn/",
-        target="_blank",
-    )
+    
 
 # Hang in there you can do it
+with ui.layout_column_wrap(fill=False):
+    with ui.value_box(showcase=icon_svg("earlybirds")):
+        "Number of penguins"
+
+        @render.text
+        def count():
+            return filtered_df().shape[0]
+
+    with ui.value_box(showcase=icon_svg("earlybirds")):
+        "Average bill length"
+
+        @render.text
+        def bill_length():
+            return f"{filtered_df()['bill_length_mm'].mean():.1f} mm"
+
+    with ui.value_box(showcase=icon_svg("earlybirds")):
+        "Average bill depth"
+
+        @render.text
+        def bill_depth():
+            return f"{filtered_df()['bill_depth_mm'].mean():.1f} mm"
+
 
 with ui.layout_columns():
-
     with ui.card(full_screen=True):
-        ui.card_header("Plotly Histogram")
+        ui.card_header("Bill Length vs. Bill Depth")
 
         @render_plotly
-        def plotly_histogram():
-            # Create a histogram using Plotly Express (aliased as px)
-            # Call px.histogram() function
-            # Pass in three arguments:
-            # the data as a pandas DataFrame (first argument, unnamed)
-            # a named argument named x set to the value returned from the input.selected_attribute() function
-            # a named argument nbins set to the user input bin count returned from the function input.plotly_bin_count() function
-            # Return the histogram created by the px.histogram() function
+        def length_depth_plotly():
             return px.histogram(
-                penguins_df, x=input.selected_attribute(), nbins=input.plotly_bin_count()
+                data_frame=filtered_df(),
+                x="bill_length_mm",
+                y="bill_depth_mm",
+                color="species",
             )
-
 
     with ui.card(full_screen=True):
-        ui.card_header("Seaborn Histogram")
+        ui.card_header("Penguin Data")
 
-        @render.plot(alt="A Seaborn histogram on penguin body mass in grams.")
-        def seaborn_histogram():
-
-            
-            histplot = sns.histplot(
-                data=penguins_df, x="body_mass_g", bins=input.seaborn_bin_count()
-            )
-            histplot.set_title("Palmer Penguins")
-            histplot.set_xlabel("Mass (g)")
-            histplot.set_ylabel("Count")
-            return histplot
-
-
-with ui.card(full_screen=True):
-    ui.card_header("Plotly Scatterplot: Species")
-
-    @render_plotly
-    def plotly_scatterplot():
-        # Almost got it 
-        
-        return px.scatter(
-            filtered_data(),
-            x="bill_length_mm",
-            y="body_mass_g",
-            color="species",
-            title="Penguins Plot (Plotly Express)",
-            labels={
-                "bill_length_mm": "Bill Length (mm)",
-                "body_mass_g": "Body Mass (g)",
-            },
-            size_max=8, # set the maximum marker size
-        )
+        @render.data_frame
+        def summary_statistics():
+            cols = [
+                "species",
+                "island",
+                "bill_length_mm",
+                "bill_depth_mm",
+                "body_mass_g",
+            ]
+            return render.DataGrid(filtered_df()[cols], filters=True)
 
 
 # It is starting to become reality
 
-
 @reactive.calc
-def filtered_data():
+def filtered_df():
+    filt_df = df[df["species"].isin(input.species())]
+    filt_df = filt_df.loc[filt_df["body_mass_g"] < input.mass()]
+    return filt_df
 
-    # The required function req() is used to ensure that
-    # the input.selected_species() function is not empty.
-    req(input.selected_species())
-
-   
-    isSpeciesMatch = penguins_df["species"].isin(input.selected_species())
-
-    # Use the boolean filter mask in square brackets to filter the DataFrame
-    # Return the filtered DataFrame when the function is triggered
-    # Filter masks can be combined with the & operator for AND and the | operator for OR
-    return penguins_df[isSpeciesMatch]
-
+@reactive.effect
+def _():
+    if input.dark_mode() == "Yes":
+        ui.update_dark_mode("dark")
+    else:
+        ui.update_dark_mode("light")
 
 # Run it and see what happens
+# Hope for the best
     
